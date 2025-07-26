0eaa863f9e51770d0d5cb288fb0edd1a6376ff15
Vanessa Ezeh <166334915+vanessaezeh8@users.noreply.github.com>
2025-07-25 22:17:59 -0700

    Worked on formatting and reseting my database due to some previous merge conflicts

--- Backend/prisma/migrations/20250716170457_removed_content/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `content` on the `StickyNote` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "StickyNote" DROP COLUMN "content";
--- Backend/prisma/migrations/20250718043835_modified_content/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "StickyNote" ALTER COLUMN "content" SET DEFAULT '';
-
--- AlterTable
-ALTER TABLE "StickyNoteVersion" ALTER COLUMN "content" SET DEFAULT '';
